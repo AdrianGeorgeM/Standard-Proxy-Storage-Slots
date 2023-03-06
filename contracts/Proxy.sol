04b62283c633f45d4ea17f348f91b5c5eb1c619a
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-03-06 19:40:14 +0000

    Update Proxy contract and delete scripts folder

---
+++ contracts/Proxy.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Proxy {
+
+}
+
+contract Logic1 {}
+
+contract Logic2 {}
